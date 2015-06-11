6925370b289c1da1a151c4fe31dd234afd9eafd6
unknown <e9001346@MNHQITLT232.ltfinc.net>
2015-06-11 10:13:45 -0500

    Initial Commit to master

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require( 'gulp' ),
+less = require( 'gulp-less' );
+
+gulp.task( 'default', [ 'less', 'move' ]);
+
+gulp.task( 'less', function() {
+	return gulp.src( './src/less/main.less' )
+		.pipe( less() )
+		.pipe( gulp.dest( './dist/css' ) );
+});
+
+var directories = [
+	'./src/js/**/*', 
+	'./src/data/*.json',
+	'./src/views/*.html',
+	'./src/index.html'
+]
+
+gulp.task( 'move', function() {
+	gulp.src( directories, { base: './src' })
+		.pipe( gulp.dest('dist/'));
+});
