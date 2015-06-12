87fcdcd821f68eb968c05a3d76fc6204e41054cc
unknown <e9001346@MNHQITLT232.ltfinc.net>
2015-06-12 11:26:03 -0500

    Completed minification and concatenaction

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ less = require( 'gulp-less' ),
 uglify = require( 'gulp-uglify' ),
 concat = require( 'gulp-concat' );
 
-gulp.task( 'default', [ 'less', 'move', 'uglify' ]);
+gulp.task( 'default', [ 'less', 'move', 'uglify', 'concat-libs' ]);
 
 gulp.task( 'less', function() {
 	return gulp.src( './src/less/main.less' )
@@ -15,19 +15,30 @@ var projectFiles = [
 	'./src/js/AngularProject.js',
 	'./src/js/controllers/*.js',
 	'./src/js/services/*.js'
-]
+];
 
 gulp.task( 'uglify', function() {
 	return gulp.src( projectFiles )
-	.pipe(concat( 'project.js' ))
-    .pipe(uglify({
+	.pipe( concat( 'project.js' ) )
+    .pipe( uglify({
         mangle: false
     }))
-    .pipe(gulp.dest('./dist/js'));
+    .pipe(gulp.dest( './dist/js' ));
+});
+
+var libraryFiles = [
+	'./src/js/libs/angular.min.js',
+	'./src/js/libs/angular-route.min.js',
+	'./src/js/libs/angular-resource.min.js'
+];
+
+gulp.task( 'concat-libs', function() {
+	return gulp.src( libraryFiles )
+	.pipe( concat( 'libraries.js' ) )
+	.pipe( gulp.dest( './dist/js' ));
 });
 
 var directories = [
-	'./src/js/libs/*.js',
 	'./src/data/*.json',
 	'./src/views/*.html',
 	'./src/index.html'
